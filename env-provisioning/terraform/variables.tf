95a3188afda12f04427b8c69b9e1da459ab39f27
Colin But <colin.but@outlook.com>
2019-10-02 22:17:21 +0100

    old unused variables

--- env-provisioning/terraform/variables.tf
+++ env-provisioning/terraform/variables.tf
@@ -3,32 +3,12 @@ variable "configuration-name" {
 }
 
 variable "region" {
-  default = "eu-west-1"
+  default     = "eu-west-1"
+  description = "The region to execute Terraform on"
 }
 
 variable "ec2_instance_type" {
   default = "t2.micro"
-}
-
-
-variable "us-east-1_AMI" {
-  default = "ami-0b898040803850657"
-}
-
-variable "us-west-1_AMI" {
-  default = "ami-056ee704806822732"
-}
-
-variable "eu-west-1_AMI" {
-  default = "ami-0bbc25e23a7640b9b"
-}
-
-variable "eu-west-2_AMI" {
-  default = "ami-0d8e27447ec2c8410"
-}
-
-variable "ap-southeast-2_AMI" {
-  default = "ami-0dc96254d5535925f"
 }
 
 variable "region_to_ami_map" {
