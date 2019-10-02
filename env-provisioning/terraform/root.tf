d3eef0fd05d9692ea73947ea56751b4ed6d1991d
Colin But <colin.but@outlook.com>
2019-10-02 20:29:33 +0100

    s3 as a backend and set required terraform version

--- env-provisioning/terraform/root.tf
+++ env-provisioning/terraform/root.tf
@@ -1,3 +1,18 @@
+terraform {
+    backend "s3" {
+        bucket = "terraform-state-colin-s3"
+        key = "terraform-state-colin-s3/path_to_my_key"
+        region = "us-east-1"
+    }
+    required_version = ">= 0.12.9"
+}
+
+
+locals {
+  region = "${lookup(var.workspace_to_region_map, terraform.workspace, "eu-west-1")}"
+}
+
+
 provider "aws" {
-  region = "${var.region}"
+  region = "${local.region}"
 }
